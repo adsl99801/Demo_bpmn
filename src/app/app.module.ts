6407c4d87edb67b73c358193663d751437e20812
keith <adsl99801@yahoo.com.tw>
2019-01-07 12:01:28 +0800

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BpmnComponent } from './bpmn/bpmn.component';
+import {HttpClientModule, HttpClient, HttpHandler} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BpmnComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
